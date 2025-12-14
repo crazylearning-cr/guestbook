948903c71da07db3be2acef362a36ad36a1f3696
Chandika <erchandika@gmail.com>
2025-12-14 17:18:50 +0530

    endtoend

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_eks_node_group" "example" {
   subnet_ids      = data.aws_subnets.default.ids
 
   scaling_config {
-    desired_size = 2
+    desired_size = 3
     max_size     = 3
     min_size     = 1
   }
